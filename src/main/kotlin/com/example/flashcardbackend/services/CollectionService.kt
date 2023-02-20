e34829e8b7398559b6b1859508d7bce63c1dfe9c
Huining Deng <huining.deng@nitor.com>
2023-02-20 22:24:00 +0200

    add Transactional annotation

--- src/main/kotlin/com/example/flashcardbackend/services/CollectionService.kt
+++ src/main/kotlin/com/example/flashcardbackend/services/CollectionService.kt
@@ -3,12 +3,15 @@ package com.example.flashcardbackend.services
 import com.example.flashcardbackend.models.DeckCollection
 import com.example.flashcardbackend.repositories.CollectionRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CollectionService(val db: CollectionRepository) {
 
+    @Transactional(readOnly = true)
     fun getCollections(): MutableIterable<DeckCollection> = db.findAll()
 
+    @Transactional
     fun post(collection: DeckCollection) {
         db.save(collection)
     }
