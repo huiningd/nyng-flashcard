21205086e7c76a99d3b95c8ad994a8d3152f8a5d
Huining Deng <huining.deng@nitor.com>
2023-03-08 23:30:42 +0200

    Add TIMESTAMP default values

--- src/main/resources/db/migration/V1__create_database.sql
+++ src/main/resources/db/migration/V1__create_database.sql
@@ -12,23 +12,23 @@ CREATE TABLE IF NOT EXISTS deck (
     group_id INTEGER NOT NULL,
     deck_name VARCHAR(50) NOT NULL,
     description VARCHAR(250),
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
     CONSTRAINT fk_deck_group FOREIGN KEY (group_id) REFERENCES deck_group (id)
 );
 
 CREATE TABLE IF NOT EXISTS card_type (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     type_name VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS card_tag (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     tag_name VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS card_content (
@@ -36,20 +36,20 @@ CREATE TABLE IF NOT EXISTS card_content (
      card_content_type enum ('FRONT', 'BACK')  NOT NULL,
      text VARCHAR(2000) NOT NULL,
      media_url VARCHAR(1000),
-     created_at TIMESTAMP NOT NULL,
-     updated_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS flashcard (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     deck_id INTEGER NOT NULL,
-    card_type_id INTEGER NOT NULL,
+    card_type_id INTEGER,
     front_content_id INTEGER NOT NULL,
-    back_content_id INTEGER NOT NULL,
+    back_content_id INTEGER,
     study_status enum ('NEW', 'YOUNG', 'MATURE', 'SUSPENDED', 'BURIED') NOT NULL,
-    last_viewed TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    last_viewed TIMESTAMP DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
     CONSTRAINT fk_flashcard_deck FOREIGN KEY (deck_id) REFERENCES deck (id),
     CONSTRAINT fk_flashcard_card_type FOREIGN KEY (card_type_id) REFERENCES card_type (id),
     CONSTRAINT fk_flashcard_front_card_content FOREIGN KEY (front_content_id) REFERENCES card_content (id) ON DELETE CASCADE,
@@ -59,24 +59,24 @@ CREATE TABLE IF NOT EXISTS flashcard (
 CREATE TABLE IF NOT EXISTS flashcard_tag (
     flashcard_id INTEGER NOT NULL,
     tag_id INTEGER NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     PRIMARY KEY (flashcard_id, tag_id),
     CONSTRAINT fk_flashcard_tag_flashcard_id FOREIGN KEY (flashcard_id) REFERENCES flashcard (id),
     CONSTRAINT fk_flashcard_tag_tag_id FOREIGN KEY (tag_id) REFERENCES card_tag (id)
 );
 
-CREATE TABLE IF NOT EXISTS card_studied_date (
+CREATE TABLE IF NOT EXISTS card_studied_datetime (
     id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     flashcard_id INTEGER NOT NULL,
-    studied_date TIMESTAMP NOT NULL,
-    CONSTRAINT fk_card_studied_date_flashcard_id FOREIGN KEY (flashcard_id) REFERENCES flashcard (id)
+    studied_datetime TIMESTAMP DEFAULT NOW() NOT NULL,
+    CONSTRAINT fk_card_studied_datetime_flashcard_id FOREIGN KEY (flashcard_id) REFERENCES flashcard (id)
 );
 
 CREATE TABLE IF NOT EXISTS comment (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     text VARCHAR(400) NOT NULL,
     flashcard_id INTEGER NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
     CONSTRAINT fk_comment_flashcard_id FOREIGN KEY (flashcard_id) REFERENCES flashcard (id)
 );
